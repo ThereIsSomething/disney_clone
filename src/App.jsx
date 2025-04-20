6275023d604f9f0d012ffcb31eabd6380d35e764
nitis <nitishbhambu07022005@gmail.com>
2025-04-20 13:51:11 +0530

    Header initialsed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello World
-      </h1>
+    < div className="">
+        <Header/>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from "../App.jsx";
+
+function Header () {
+    return (
+        <div>Header</div>
+    )
+}
+
+export default Header;
