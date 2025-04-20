1cda1482c6f7dc83755f93f845ddc4266d0767e0
nitis <nitishbhambu07022005@gmail.com>
2025-04-20 15:36:54 +0530

    PIXAR, Disney etc cards added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Slider from './components/Slider'
+import ProductionHouse from "./components/ProductionHouse.jsx";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     < div className="">
         <Header/>
         <Slider/>
+        <ProductionHouse/>
     </div>
   )
 }
