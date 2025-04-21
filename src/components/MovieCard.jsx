3efa3f2e28607c290877fa8213a7341fefda7e75
nitis <nitishbhambu07022005@gmail.com>
2025-04-21 18:08:47 +0530

    implemented context API + routed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+import React, { useContext } from 'react';
+import { WishlistContext } from '../context/WishlistContext';
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
-function MovieCard({movie}) {
+function MovieCard({ movie }) {
+    const { addToWishlist } = useContext(WishlistContext);
+
+    const handleAddToWishlist = () => {
+        addToWishlist(movie);  // Add the clicked movie to the wishlist
+    };
+
     return (
         <>
-            <img src={IMAGE_BASE_URL+movie.poster_path}
-                 className='w-[110px] md:w-[200px] rounded-lg
-        hover:border-[3px] border-gray-400 cursor-pointer
-        hover:scale-110 transition-all duration-150 ease-in'/>
+            <img
+                src={IMAGE_BASE_URL + movie.poster_path}
+                className="w-[110px] md:w-[200px] rounded-lg hover:border-[3px] border-gray-400 cursor-pointer hover:scale-110 transition-all duration-150 ease-in"
+                onClick={handleAddToWishlist}  // Add to wishlist on click
+            />
         </>
-    )
+    );
 }
 
-export default MovieCard
+export default MovieCard;
