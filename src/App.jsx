d9ff869954ba0bc53a9e531c4e96578d3aa24d38
nitis <nitishbhambu07022005@gmail.com>
2025-04-21 18:07:03 +0530

    react router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Slider from './components/Slider'
-import ProductionHouse from "./components/ProductionHouse.jsx";
-import GenreMovieList from "./components/GenreMovieList.jsx";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Slider from './components/Slider';
+import ProductionHouse from './components/ProductionHouse';
+import GenreMovieList from './components/GenreMovieList';
+import Wishlist from './components/Wishlist';
+import { WishlistProvider } from './context/WishlistContext.jsx'; // Import WishlistProvider
 
 function App() {
-
-  return (
-    < div className="">
-        <Header/>
-        <Slider/>
-        <ProductionHouse/>
-        <GenreMovieList/>
-    </div>
-  )
+    return (
+        <WishlistProvider>
+            <Router>
+                <div className="">
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={
+                            <>
+                                <Slider />
+                                <ProductionHouse />
+                                <GenreMovieList />
+                            </>
+                        } />
+                        <Route path="/wishlist" element={<Wishlist />} />
+                    </Routes>
+                </div>
+            </Router>
+        </WishlistProvider>
+    );
 }
 
-export default App
+export default App;
